6900ac609a2bf9987504d3c9694e2ff877f7fbb3
Sven Krefeld <info@svenkrefeld.de>
2025-03-20 18:51:42 +0100

    basic html structure for default template

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,11 @@
+<!doctype html>
+<html lang="de-DE">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $page->title() ?></title>
+</head>
+<body>
 <h1><?= $page->title() ?></h1>
+</body>
+</html>
