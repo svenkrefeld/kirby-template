00b551efd0c021e5a7efd473488399f06657f737
Sven Krefeld <info@svenkrefeld.de>
2025-03-20 19:02:31 +0100

    basic tailwind integration

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page->title() ?></title>
+    <?= css('assets/css/styles.css') ?>
 </head>
 <body>
-<h1><?= $page->title() ?></h1>
+<h1 class="text-3xl font-bold underline"><?= $page->title() ?></h1>
 </body>
 </html>
